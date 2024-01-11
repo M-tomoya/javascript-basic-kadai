ef65b81126c32ef972fc5515a9f1cd1507a72442
M-tomoya <155372188+M-tomoya@users.noreply.github.com>
2024-01-11 12:14:54 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,12 @@
+// 変数の宣言
+let bodyWeight;
+let bodyHeight;
+let bodymassIndex;
+
+// 値の代入
+bodyWeight = '68';
+bodyHeight = '1.7';
+bodymassIndex = (bodyWeight / (bodyHeight * bodyHeight));
+
+// コンソールへの出力
+console.log(bodymassIndex);
